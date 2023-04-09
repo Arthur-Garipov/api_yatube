cfe1fcde5a9f89e3bfcf595e495a031b198b8b1d
Garipov Arthur <gam1997.05.31@gmail.com>
2023-04-09 14:27:04 +0300

    Подготовлено к ревью

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -23,37 +23,3 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'author', 'post', 'text', 'created')
-
-
-# from rest_framework import serializers
-# from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
-# from posts.models import Comment, Post, Group
-
-
-# class GroupSerializer(serializers.ModelSerializer):
-#     author = SlugRelatedField(slug_field='username', read_only=True)
-#     group = SlugRelatedField(slug_field='slug', read_only=True)
-
-#     class Meta:
-#         model = Group
-#         fields = '__all__'
-
-
-# class PostSerializer(serializers.ModelSerializer):
-#     author = SlugRelatedField(slug_field='username', read_only=True)
-#     group = serializers.SlugRelatedField(
-#         slug_field='slug', queryset=Group.objects.all(), required=False
-#     )
-
-#     class Meta:
-#         fields = '__all__'
-#         model = Post
-
-
-# class CommentSerializer(serializers.ModelSerializer):
-#     author = SlugRelatedField(slug_field='username', read_only=True)
-#     post = PrimaryKeyRelatedField(read_only=True)
-
-#     class Meta:
-#         fields = '__all__'
-#         model = Comment
